3c371c53fb3dd8a9ee6c66e7c58f0523fde849d0
Anand Pratap Singh <42322585+anand018@users.noreply.github.com>
2023-12-30 11:24:58 +0530

    initial commit

---
+++ src/main/java/com/simlearn/instructormanager/InstructorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.simlearn.InstructorManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorManagerApplication.class, args);
+	}
+
+}
